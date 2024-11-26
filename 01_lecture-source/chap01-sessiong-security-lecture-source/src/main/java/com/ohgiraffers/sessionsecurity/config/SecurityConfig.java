7a886b9b1989e25f0072d4e9a4084e78dae7017f
Yongwook <kyw9885@gmail.com>
2024-11-26 15:49:45 +0900

    [1126] security

--- 01_lecture-source/chap01-sessiong-security-lecture-source/src/main/java/com/ohgiraffers/sessionsecurity/config/SecurityConfig.java
+++ 01_lecture-source/chap01-sessiong-security-lecture-source/src/main/java/com/ohgiraffers/sessionsecurity/config/SecurityConfig.java
@@ -2,13 +2,19 @@ package com.ohgiraffers.sessionsecurity.config;
 
 import com.ohgiraffers.sessionsecurity.common.UserRole;
 import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import java.nio.file.PathMatcher;
 
 @Configuration
 /* comment. spring security 기능 활성화를 위한 어노테이션 */
@@ -25,9 +31,17 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder(); // 보안이 빠르고 안전
     }
 
+    // 정적 리소스에 대한 요청은 시큐리티 설정이 돌지 못 하게 설정
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+
+    @Autowired
+    private AuthFailHandler authFailHandler;
+
     /* comment. 여기가 설정의 핵심 */
     @Bean
-    public SecurityFilterChain configure(HttpSecurity http) {
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         // 서버의 리소스 접근 가능 권한 설정
         http.authorizeHttpRequests(auth -> {
             // permitAll() -> 인증 되지 않은(로그인 되지 않은) 사용자들이 접근할 수 있는 URL 기술
@@ -42,7 +56,32 @@ public class SecurityConfig {
             login.loginPage("/auth/login");  // 로그인 페이지 url 을 기술
             // 사용자가 ID 를 입력하는 필드(input 타입 name 과 반드시 일치해야 한다.)
             login.usernameParameter("user");
-        })
+            // 사용자가 PWD 를 입력하는 필드(input 타입 name 과 반드시 일치)
+            login.passwordParameter("pass");
+            // 사용자가 로그인에 성공 했을 시 보내줄 URL 기술
+            login.defaultSuccessUrl("/", true);
+            // 로그인에 실패 했을 시 내용을 기술한 객체 호출 아직 미작성
+            login.failureHandler(authFailHandler);
+        }).logout(logout -> {
+            // 로그아웃을 담당할 핸들러 메소드 요청 URL 기술
+            logout.logoutRequestMatcher(new AntPathRequestMatcher("/auth/logout"));
+            // session 은 쿠키 방식으로 저장이 되어 있어 로그인을 하면
+            // 해당하는 쿠키를 삭제함으로서 로그아웃을 만들어준다.
+            logout.deleteCookies("JSESSIONID");
+            // 서버측의 세션 공간 만료
+            logout.invalidateHttpSession(true);
+            // 로그아웃 성공 시 요청 URL 기술
+            logout.logoutSuccessUrl("/");
+        }).sessionManagement(session -> {
+            session.maximumSessions(1); // session 의 허용 갯수 제한
+            // 한 사용자가 여러 창을 띄워 동시에 세션 여러 개 활성화 방지
+            // 세션이 만료 되었을 때 요청할 URL 기술
+            session.invalidSessionUrl("/");
+            // 추가적인 구현이 필요하므로 비활성화
+        }).csrf(csrf -> csrf.disable());
+
+        // 위에서 설정한 내용대로 시큐리티 기능 빌드(생성)
+        return http.build();
      }
 
 }
--- 01_lecture-source/chap01-sessiong-security-lecture-source/src/main/java/com/ohgiraffers/sessionsecurity/user/controller/MainController.java
+++ 01_lecture-source/chap01-sessiong-security-lecture-source/src/main/java/com/ohgiraffers/sessionsecurity/user/controller/MainController.java
@@ -2,6 +2,7 @@ package com.ohgiraffers.sessionsecurity.user.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MainController {
@@ -12,5 +13,18 @@ public class MainController {
 
     }
 
+    /* comment. 관리자만 들어갈 수 있는 지 TEST */
+    @GetMapping("/admin/page")
+    public ModelAndView adminPage(ModelAndView mv) {
+        mv.setViewName("admin/admin");
+        return mv;
+    }
+
+    /* comment. 유저만 들어갈 수 있는 지 TEST */
+    @GetMapping("/user/page")
+    public ModelAndView userPage(ModelAndView mv) {
+        mv.setViewName("user/user");
+        return mv;
+    }
 
 }
