9a604f3857a40220c80cf7afdc2c6bcfd7011ddc
Yongwook <kyw9885@gmail.com>
2024-11-26 10:02:48 +0900

    [1126] security

---
+++ 01_lecture-source/chap01-sessiong-security-lecture-source/src/main/java/com/ohgiraffers/sessiongsecurity/config/ContextConfig.java
@@ -0,0 +1,4 @@
+package com.ohgiraffers.sessiongsecurity.config;
+
+public class ContextConfig {
+}
---
+++ 01_lecture-source/chap01-sessiong-security-lecture-source/src/test/java/com/ohgiraffers/sessiongsecurity/Chap01SessiongSecurityLectureSourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.sessiongsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chap01SessiongSecurityLectureSourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
